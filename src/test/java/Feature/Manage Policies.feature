a38a89ed1909a514bf6d99b23081d631727a752a
Nandkumar Babar <nandkumar.babar@steerlean.com>
2021-06-19 15:12:53 +0530

    Cucumber

--- src/test/java/Feature/Manage Policies.feature
+++ src/test/java/Feature/Manage Policies.feature
@@ -1,7 +1,9 @@
 Feature: Manage Policies
+
   Scenario: add and remove policies
-    Given Employer Remove policy
+    Given Employer should be on policy page
     Then Employer Flip policy
     Then Employer View policy
+    Then Employer Remove policy
     Then Employer Add policy
     Then Employer Edit policy
--- src/test/java/Steps/ManagePolicies.java
+++ src/test/java/Steps/ManagePolicies.java
@@ -13,7 +13,7 @@ public class ManagePolicies {
     BaseStep step= new BaseStep();
     WebDriver driver;
 
-    @Given("Employer Remove policy")
+    @Given("Employer should be on policy page")
     public void remove_user() throws InterruptedException {
 
         step.BaseStep();
@@ -26,14 +26,7 @@ public class ManagePolicies {
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div/div/div/div/div[2]/form/button"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/main/div[1]/div[2]/div[1]/div/div[2]/nav/ul/li[2]/div[1]/span[3]"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/main/div[1]/div[2]/div[1]/div/div[2]/nav/ul/li[2]/div[2]/div/ul/li[2]/div/span"))).click();
-        //Remove policy
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/main/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[1]/div/div[4]/div/div/button[4]"))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[1]/div/div/div[2]/div[2]/button[1]"))).click();
-        Thread.sleep(5000);
     }
-
-
-
     @Then("Employer Flip policy")
     public void add_the_user() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(step.driver, BaseStep.TimeOut.CUSTOM_MAX.getValue());
@@ -42,25 +35,30 @@ public class ManagePolicies {
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[1]/div/div/div[2]/form/div/div[2]/div/button"))).click();
         Thread.sleep(4000);
     }
-
     @Then("Employer View policy")
     public void edit_the_user() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(step.driver, BaseStep.TimeOut.CUSTOM_MAX.getValue());
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/main/div[2]/div[1]/div[2]/div/div[1]/div[2]/div[1]/div/div[4]/div/div/button[1]"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[1]/div/div/div[1]/button/span"))).click();
     }
+    @Then("Employer Remove policy")
+    public void removepolicy() throws InterruptedException {
+        //Remove policy
+        WebDriverWait wait = new WebDriverWait(step.driver, BaseStep.TimeOut.CUSTOM_MAX.getValue());
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//button[@class=\"noButtonBorder\"])[4]"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[1]/div/div/div[2]/div[2]/button[1]"))).click();
+        Thread.sleep(5000);
+    }
 
     @Then("Employer Add policy")
     public void flip_policies() throws InterruptedException {
         Thread.sleep(5000);
-        // step.findElementClick("//*[@id=\"root\"]/main/div[2]/div[1]/button/span[2]",xPath);
         WebDriverWait wait = new WebDriverWait(step.driver, BaseStep.TimeOut.CUSTOM_MAX.getValue());
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/main/div[2]/div[1]/div[3]/button"))).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.name("title"))).sendKeys("policy no 99");
+        wait.until(ExpectedConditions.elementToBeClickable(By.name("title"))).sendKeys("Policy 3");
         WebElement browse = step.driver.findElement(By.xpath("//input[@name='policyDoc']"));
         browse.sendKeys("/home/nandkumar/Downloads/Jacky-Smith-Resume-Project-Manager-3.pdf");
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']"))).click();
-
     }
     @Then("Employer Edit policy")
     public void Edit_policy() throws InterruptedException {
